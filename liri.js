441a55780d455268ac8dc847a7b6ba75b25a0f66
Paul Unsted <ed__84@hotmail.com>
2019-07-29 18:11:00 -0400

    Ok, wrote search functions for OMDB and BandsInTown, along with some conditional functioning like giving users a choice to exit the program or calling them Stranger if they don't input a name. I'm actually quite impressed with how far along I've come in a few hours. That being said, it's time for a break.

--- liri.js
+++ liri.js
@@ -1,74 +1,144 @@
+// Node packages
+
 require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var axios = require("axios");
+
 var inquirer = require("inquirer");
 
+var fs = require("fs");
+
 var spotify = keys.spotify;
+
+// Prompt for users to input their name, and chose what function they want to run.
 
 inquirer
     .prompt([
         {
-            type:"input",
-            message:"What's your name, stranger?",
-            name:"userName"
+        type:"input",
+        message:"What's your name, stranger?",
+        name:"userName"
         },
         {
-            type:"list",
-            message:"What would you like to do?",
-            choices:["Search for music", "Search for concerts", "Search for movies", "None of the above! Get me out of here!"],
-            name:"userChoice"
+        type:"list",
+        message:"What would you like to do?",
+        choices:["Search for music", "Search for concerts", "Search for movies", "None of the above! Get me out of here!"],
+        name:"userChoice"
         }
     ])
     .then(function(inquirerResponse){
         
+        if(inquirerResponse.userName === ""){
+            console.log("\nMysterious one, are you? I guess I'll stick to calling you Stranger.")
+            inquirerResponse.userName = "Stranger";
+        }
+
         if (inquirerResponse.userChoice === "Search for music"){
             function music(){
                 inquirer
                     .prompt([
                         {
-                            type:"input",
-                            message:"What song would you like to search for, " + inquirerResponse.userName + "?",
-                            name:"songName"
-                        }
-                    ])
-                    .then(function(inquirerResponse){
-                        console.log(inquirerResponse);
-                    })
-            }
-            music();
-        }
-        if(inquirerResponse.userChoice === "Search for concerts"){
-            function concert(){
-                inquirer
-                    .prompt([
-                        {
-                            type:"input",
-                            message:"Whose concert would you like to search for, " + inquirerResponse.userName + "?",
-                            name:"concertName"
+                        type:"input",
+                        message:"What song would you like to search for, " + inquirerResponse.userName + "?",
+                        name:"songName"
                         }
                     ])
                     .then(function(inquirerResponse){
                         console.log(inquirerResponse);    
                     })
             }
+            music();
+
+        // Concert search, using BandsInTown
+        
+        }
+        if(inquirerResponse.userChoice === "Search for concerts"){
+            function concert(){
+                inquirer
+                    .prompt([
+                        {
+                        type:"input",
+                        message:"Whose concerts would you like to search for, " + inquirerResponse.userName + "?",
+                        name:"bandName"
+                        }
+                    ])
+                    .then(function(inquirerResponse){
+                        axios.get("https://rest.bandsintown.com/artists/" + inquirerResponse.bandName + "/events?app_id=codingbootcamp").then(function(response){
+                            console.log(response.data);
+                            if(response.data === []){
+                                console.log("\nI'm sorry, we couldn't find any concerts for " + inquirerResponse.bandName + ". :<");
+                            }
+                            for(i=0; i < response.data.length; i++){
+                                
+                                console.log(response.data[i]);
+                                console.log("\nVenue: " + response.data[i].venue.name);
+                                console.log("\nLocation: " + response.data[i].venue.city + ", " + response.data[i].venue.country + ".");
+                                console.log("\nDate: " + response.data[i].datetime);
+                                console.log("\n---------------------------")
+                            }
+                            
+                        })
+                    })
+            }
             concert();
         }
+
+        // Movie search, using OMDB API
+
         if(inquirerResponse.userChoice === "Search for movies"){
             function movie(){
                 inquirer
                     .prompt([
                         {
-                            type:"input",
-                            message:"What movie would you like to search for, " + inquirerResponse.userName + "?",
-                            name: "movieName"
+                        type:"input",
+                        message:"What movie would you like to search for, " + inquirerResponse.userName + "?",
+                        name: "movieName"
                         }
                     ])
                     .then(function(inquirerResponse){
-                        console.log(inquirerResponse);
+
+                        if(inquirerResponse.movieName === ""){
+                            inquirerResponse.movieName = "Mr. Nobody";
+                        }
+                        axios.get("http://www.omdbapi.com/?t=" + inquirerResponse.movieName + "&y=&plot=short&apikey=trilogy").then(function(response){
+                            console.log("\nThis title of the movie is: " +response.data.Title);
+                            console.log("\nIt was released in: " + response.data.Year);
+                            console.log("\nIts IMDB rating is: " + response.data.imdbRating);
+                            console.log("\nIts Rotten Tomatoes rating is: " + response.data.Ratings[1]);
+                            console.log("\nIt was produced in: " + response.data.Country);
+                            console.log("\nIts language is: " + response.data.Language);
+                            console.log("\nPlot: " + response.data.Plot);
+                            console.log("\nStarring: " + response.data.Actors);
+                        })
+                        .catch(function(error) {
+                            if (error.response) {
+                            // The request was made and the server responded with a status code
+                            // that falls out of the range of 2xx
+                            console.log("---------------Data---------------");
+                            console.log(error.response.data);
+                            console.log("---------------Status---------------");
+                            console.log(error.response.status);
+                            console.log("---------------Status---------------");
+                            console.log(error.response.headers);
+                            } else if (error.request) {
+                            // The request was made but no response was received
+                            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                            console.log(error.request);
+                            } else {
+                            // Something happened in setting up the request that triggered an Error
+                            console.log("Error", error.message);
+                            }
+                            console.log(error.config);
+                        });
+                        
                     })
             }
             movie();
+
+        // Exit option for those that don't want to run any searches.
+        
         }
         if(inquirerResponse.userChoice === "None of the above! Get me out of here!"){
             console.log("\nSuit yourself, " + inquirerResponse.userName + ". Come back if you change your mind!");
